bf9ae7491984e48a7834954a298bfab8ee3bfb67
Anna Brekka <annaebr@stud.ntnu.no>
2025-05-20 10:27:42 +0200

    laget klasser av mean, median og std

--- src/Average.py
+++ src/Average.py
@@ -1,21 +1,54 @@
-
-class FindAverage:
-
+# Oppretter en klasse som definerer en ny dataframe og filtrer og år og måned
+class Data:
     def __init__(self, data):
         self.df = data
 
-    def mean_per_year(self, parameter, unit):
+    def prepare_data(self, parameter):
         # Lager en ny datafram som kun inneholder en parameter
-        df_parameter = self.df[self.df["elementId"] == parameter].copy()
+        df_param = self.df[self.df["elementId"] == parameter].copy()
         # Filtrerer ut år og måned
-        df_parameter.loc[:, "year"] = df_parameter["date"].dt.year
-        df_parameter.loc[:, "month"] = df_parameter["date"].dt.month
+        df_param.loc[:, "year"] = df_param["date"].dt.year
+        df_param.loc[:, "month"] = df_param["date"].dt.month
+        return df_param.groupby(["year", "month"])
+
+
+# Oppretter en klasse som finner gjennomsnittet for de tre ulike parameterene, denne klassen bruker også klassen "Data"
+class FindAverage(Data):
+
+    def mean_per_year(self, parameter, unit):
         # Finner gjennomsnittet per måned per år, og runder av til 3 desimaler
-        monthly_average_per_year = df_parameter.groupby(["year", "month"])["value"].mean().reset_index()
-        monthly_average_per_year["value"] = monthly_average_per_year["value"].round(3)  
+        grouped = self.prepare_data(parameter)
+        monthly_average_per_year = grouped["value"].mean().reset_index()
+        monthly_average_per_year["value"] = monthly_average_per_year["value"].round(3) 
         monthly_average_per_year.to_csv(f"../data/Avarage/average_{unit}.csv")
         print(f"Gjennomsnittet for {unit}")  
         print(monthly_average_per_year.head())
+
+
+# Oppretter en klasse som finner medianen for de tre ulike parameterene, denne klassen bruker også klassen "Data"
+class FindMedian(Data):
+    
+    def median_per_year(self, parameter, unit):
+        grouped = self.prepare_data(parameter)
+        monthly_median_per_year = grouped["value"].median().reset_index()
+        monthly_median_per_year["value"] = monthly_median_per_year["value"].round(3)
+        monthly_median_per_year.to_csv(f'../data/Avarage/median_{unit}.csv')
+        print(f"Median for {unit}")
+        print(monthly_median_per_year.head())
+
+
+# Oppretter en klasse som finner standardavik for de tre ulike parameterene, denne klassen bruker også klassen "Data"
+class FindStd(Data):
+
+    def std_per_year(self, parameter, unit):
+        grouped = self.prepare_data(parameter)
+        monthly_std_per_year = grouped["value"].std().reset_index()
+        monthly_std_per_year["value"] = monthly_std_per_year["value"].round(3)
+        monthly_std_per_year.to_csv(f'../data/Avarage/std_{unit}.csv')
+        print(f"Standardavvik for {unit}")
+        print(monthly_std_per_year.head())
+        
+
 
 
 
